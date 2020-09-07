9ea4c6ea7a5ad94ecde8358fa310e90f144b5246
Tkay-Kim <59009055+Tkay-Kim@users.noreply.github.com>
2020-09-07 23:35:14 +0900

    rtest

---
+++ rtest.R
@@ -0,0 +1,26 @@
+names(help)
+
+
+
+
+  help %>% group_by(x) %>% summarise(exp(mean(log_rf)),exp(sd(log_rf)))
+  help %>% group_by(x) %>% summarise(exp(mean(log_rf)),exp(sd(log_scl70_1)))
+  help %>% group_by(x) %>% summarise(exp(mean(log_rf)),exp(sd(log_sm1)))
+  help %>% group_by(x) %>% summarise(exp(mean(log_rf)),exp(sd(log_ssb1)))
+  help %>% group_by(x) %>% summarise(exp(mean(log_rf)),exp(sd(log_smrnp1)))
+  help %>% group_by(x) %>% summarise(exp(mean(log_rf)),exp(sd(log_ssa)))
+  help %>% group_by(x) %>% summarise(exp(mean(log_rf)),exp(sd(log_ana)))
+  help %>% group_by(x) %>% summarise(exp(mean(log_rf)),exp(sd(log_anca)))
+
+
+
+
+1*(1.1)^30
+  
+  
+
+
+
+
+
+
